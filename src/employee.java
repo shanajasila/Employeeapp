e72a1afc63aadfc128fdd9dddd51fcb9c9c5836a
shanajasila <shanajasilam@gmail.com>
2022-11-21 15:18:18 +0530

    created

--- src/employee.java
+++ src/employee.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class employee {
+    public static void main(String args[]){
+        while (true){
+            System.out.println("Select the options");
+            Scanner sc=new Scanner(System.in);
+            System.out.println("1.Add Employee\n2.View Employee\n3.Search Employee\n4.Delete Employee\n5.Exit");
+
+        }
+    }
+
+
 }
